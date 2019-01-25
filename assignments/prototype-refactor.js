919c7abd45cb335a5095b9da421ce07f1a115b08
Marcelo Laos <mlaos1997@gmail.com>
2019-01-24 21:53:43 -0500

    update

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -3,7 +3,7 @@
 Prototype Refactor
 
 1. Copy and paste your code or the solution from yesterday
-
+ 1. copy and paste your code.
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
